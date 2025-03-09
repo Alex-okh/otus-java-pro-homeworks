3abdbea8565ce8a29f426b97ba627c36f1684bdd
Alexey Okhrimenko <alexey_nats@mail.ru>
2025-03-09 12:17:38 +0300

    HomeWork1 - Устранение замечаний

--- pro-homeworks/hw01-gradle/build.gradle.kts
+++ pro-homeworks/hw01-gradle/build.gradle.kts
@@ -2,34 +2,20 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     id("java")
-    id("com.github.johnrengelman.shadow") version "8.1.1" // Add the Shadow plugin
-}
-
-group = "ru.otus"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("com.google.guava:guava:33.4.0-jre")
+   implementation("com.google.guava:guava")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
-// Configure the ShadowJar task
 tasks {
     named<ShadowJar>("shadowJar") {
         archiveBaseName.set("HelloOtus")
         archiveVersion.set("0.1")
         archiveClassifier.set("")
         manifest {
-            attributes["Main-Class"] = "HelloOtus"
+            attributes["Main-Class"] = "hello.HelloOtus"
         }
     }
 
