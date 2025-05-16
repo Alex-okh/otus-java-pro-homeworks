256bf18679f3efb63aa626063271c64a38c3c799
Alexey Okhrimenko <alexey_nats@mail.ru>
2025-05-16 14:14:43 +0300

    HomeWork 08 - File open fixed.

--- pro-homeworks/hw08-json-handler/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ pro-homeworks/hw08-json-handler/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -2,23 +2,33 @@ package ru.otus.dataprocessor;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
 import ru.otus.model.Measurement;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
 public class ResourcesFileLoader implements Loader {
-    private final String fileName;
+  private final String fileName;
 
-    public ResourcesFileLoader(String fileName) {
+  public ResourcesFileLoader(String fileName) {
     this.fileName = fileName;
+  }
+
+  @Override
+  public List<Measurement> load() throws IOException {
+    File inputFile;
+    try {
+      var resource = ResourcesFileLoader.class.getClassLoader().getResource(fileName);
+      inputFile = new File(resource.toURI());
+    } catch (URISyntaxException | NullPointerException ex){
+      throw new FileProcessException("Cannot find/access file <%s>".formatted(fileName));
     }
 
-    @Override
-    public List<Measurement> load() throws IOException {
-        File inputFile = new File("src/test/resources/" + fileName);
-        ObjectMapper mapper = new ObjectMapper();
+    ObjectMapper mapper = new ObjectMapper();
 
-        return mapper.readValue(inputFile, new TypeReference<List<Measurement>>(){});
-    }
+    return mapper.readValue(inputFile, new TypeReference<List<Measurement>>() {
+    });
+  }
 }
