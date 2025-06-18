07c8915a27220e8fb38c9cfdde7dc4cc9bf56835
Alexey Okhrimenko <alexey_nats@mail.ru>
2025-06-18 16:47:39 +0300

    HW12 запустилось :-)

---
+++ pro-homeworks/hw12-web/src/main/java/ru/otus/servlet/ClientsServlet.java
@@ -0,0 +1,36 @@
+package ru.otus.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import ru.otus.crm.model.Client;
+import ru.otus.crm.service.DBServiceClient;
+import ru.otus.services.TemplateProcessor;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ClientsServlet extends HttpServlet {
+    private static final String CLIENTS_PAGE_TEMPLATE = "clients.html";
+    private final transient DBServiceClient clientService;
+    private final transient TemplateProcessor templateProcessor;
+
+    public ClientsServlet(DBServiceClient clientService, TemplateProcessor templateProcessor) {
+        this.clientService = clientService;
+        this.templateProcessor = templateProcessor;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, Object> modelParams = new HashMap<>();
+        List<Client> clients = clientService.findAll();
+        modelParams.put("clients", clients);
+        resp.setContentType("text/html");
+        resp.getWriter().println(templateProcessor.getPage(CLIENTS_PAGE_TEMPLATE, modelParams));
+
+    }
+}
