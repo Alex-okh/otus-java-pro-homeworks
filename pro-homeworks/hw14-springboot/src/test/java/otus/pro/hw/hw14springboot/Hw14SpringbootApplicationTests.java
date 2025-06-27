6697378e5c08fe04f66f56db090735a28ccee270
Alexey Okhrimenko <alexey_nats@mail.ru>
2025-06-27 10:37:30 +0300

    HW14 INIT.

---
+++ pro-homeworks/hw14-springboot/src/test/java/otus/pro/hw/hw14springboot/Hw14SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package otus.pro.hw.hw14springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw14SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
